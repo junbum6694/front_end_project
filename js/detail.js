da7db7ef0a65d90144301cfe6d2e9da4db181037
Tayo Bus <wnsqja6694@gmail.com>
2025-07-22 18:03:28 +0900

    Feat: 댓글 작성 기능 추가
    로그인 한 사용자 데이터 authentication 추가, 댓글 작성 후 제출 시 댓글목록에 추가되는 기능 추가

--- js/detail.js
+++ js/detail.js
@@ -65,8 +65,42 @@ const videos = [
   },
 ];
 
+const authentication = {
+  name: "박찬호",
+  nickname: "TooMuchTalker",
+  profile: "../images/profile.png",
+};
+
 const params = new URLSearchParams(location.search);
 const id = params.get("id");
 const matched = videos.find((video) => video.id == id);
 
 $("#video").attr("src", matched.url);
+
+function writeComment(comment) {
+  $("#comment-list").prepend(`
+<div class="comment-container d-flex align-items-center col-10 pb-4 mb-4">
+    <div class="comment-image">
+        <img src="${authentication.profile}" id="commenter-profile" alt="프로필 이미지" width="40" height="40" class="d-inline-block align-text-top me-3 rounded-circle" />
+    </div>
+
+    <div class="comment mb-0 col-11">
+        <span>${authentication.nickname}</span><br />
+        <span>${comment}</span>
+    </div>
+</div>
+  `);
+}
+
+$("#comment-form").on("submit", function (e) {
+  e.preventDefault();
+
+  const comment = $("#comment-input").val().trim();
+
+  if (comment === "") {
+    window.alert("댓글을 입력해 주세요.");
+  } else {
+    writeComment(comment);
+    $("#comment-input").val("");
+  }
+});
